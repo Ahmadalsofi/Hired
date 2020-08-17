ba80917b9c75efab5b13c874d01f8481d8766acb
Zeeshan Khan <zeeshan.s.khan@gmail.com>
2020-08-17 07:50:12 +0400

    Add project around Hired and a podspec for pod install

---
+++ Sources/HiredManager.swift
@@ -0,0 +1,15 @@
+//
+//  HiredManager.swift
+//  Hired
+//
+//  Created by Zeeshan Khan on 17/08/2020.
+//  Copyright © 2020 Zeeshan Khan. All rights reserved.
+//
+
+import Foundation
+
+public class HiredManager {
+    public init() {
+        
+    }
+}
